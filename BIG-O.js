67b86cb0e64bfc447623461ac8fc6cbf658fcd3e
azacdev <azacdev@gmail.com>
2023-06-24 23:36:59 +0100

    big O explanation updated

---
+++ BIG-O.js
@@ -0,0 +1,33 @@
+// Big-O Worst case complexity of an algorithm is represented using Big-O notation 
+// // O(1) constant Time / Space Complexity
+// function summation2 (n) {
+//   return (n * (n + 1 )) / 2 // ---------- 1
+// }
+
+// // O(n) Linear Time / Space Complexity
+// function summation(n) {
+//   let sum = 0 //--------------------- 1
+//   for(i = 1; i <= n; i++) {
+//     sum += i //---------------------- 4   n + 2
+// }
+//   return sum  // -------------------- 1
+// }
+
+// // O(n ^ 2) Quadratic Time / Space Complexity
+// for (i = 0; i <= n; i++) {
+//   for (j = 0; j <= i; j++) {
+//     // some code
+//   }
+// }
+
+// // O(n ^ 3) Cubic Time / Space Complexity
+// for (i = 0; i <= n; i++) {
+//   for (j = 0; j <= i; j++) {
+//     for (k = 0; k <= j; k++) {
+//       //some code
+//     }
+//   }
+// }
+
+// // O(logn) Logarithmic Time Space Complexity
+// // Input size reduces by half every iteration
