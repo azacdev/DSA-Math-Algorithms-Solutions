7b034db213fa43f4f6a8ba56903cffbd5a6bd578
azacdev <azacdev@gmail.com>
2023-06-24 23:26:32 +0100

    power of two explanation updated

--- power-of-two.js
+++ power-of-two.js
@@ -1,5 +1,6 @@
 // POWER OF TWO
-// An interger is a power of two if there exist an interger "X" sunch that n === 2^x
+// An interger is a power of two if there exist an interger "X" such that n === 2^x
+// e.g 2,4,8,16,32
 
 // e.g
 // n = 8
@@ -20,11 +21,11 @@ function isPowerOfTwo(n) {
   if (n < 1) {
     return false;
   }
-  while (n > 1) {
-    if (n % 2 !== 0) {
+  while (n > 1) { // n will get divider by 2 if n is greater than 1 the loops keep going
+    if (n % 2 !== 0) { // if the remainder is not equal to zero n is not a power of two
       return false;
-    }
-    n = n / 2;
+    }  // If n is divisible by 2 (i.e., n % 2 equals 0), the code proceeds to divide n by 2 using the assignment n = n / 2. This step effectively reduces n by half.If n is divisible by 2 (i.e., n % 2 equals 0), the code proceeds to divide n by 2 using the assignment n = n / 2.
+    n = n / 2  // Once n reaches a value of 1, it means that the original input n was a power of two. At this point, the loop exits, and the function returns true.
   }
   return true;
 }
